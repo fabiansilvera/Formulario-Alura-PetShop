67550a29caf48b1d8ade212facfafef9f2fe7f21
angel fabian silvera <108084824+fabiansilvera@users.noreply.github.com>
2022-10-17 08:17:28 -0300

    Commit Inicial

---
+++ js/validaciones.js
@@ -0,0 +1,91 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if(validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+
+    if(input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = "";
+    }else {
+        input.parentElement.classList.add("input-container--invalid");
+        input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeError(tipoDeInput, input);
+    }
+}
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch", 
+    "customError"
+]
+
+const mensajesDeError = {
+    nombre: {
+        valueMissing: "este campo no puede estar vacio"
+    },
+    email: {
+        valueMissing: "este campo no puede estar vacio",
+        typeMismatch: "El correo no es valido"
+    },
+    password: {
+        valueMissing: "este campo no puede estar vacio",
+        patternMismatch:"Al menos 6 caracteres, maximo 12, debe contener una letra minuscula, una letra mayuscula, un numero y no puede contener caracteres especiales."
+    },
+    nacimiento: {
+        valueMissing: "este campo no puede estar vacio",
+        customError: "Debes tener al menos 18 años de edad" 
+    },
+    numero: {
+        valueMissing: "este campo no puede estar vacio",
+        patternMismatch: "El formato requerido es de 8 a 12 digitos"
+    },
+    cep: {
+        valueMissing: "este campo no puede estar vacio",
+        patternMismatch: "la direccion debe contener entre 10 a 40 caracteres."
+    },
+    ciudad: {
+        valueMissing: "este campo no puede estar vacio",
+        patternMismatch: "la ciudad debe contener entre 5 a 40 caracteres."
+    },
+    provincia: {
+        valueMissing: "este campo no puede estar vacio",
+        patternMismatch: "la provincia debe contener entre 5 y 40 caracteres."
+    }
+}
+
+const validadores = {
+    nacimiento: (input) => validarNacimiento(input),
+};
+
+function mostrarMensajeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach((error) => {
+        if (input.validity[error]) {
+            mensaje = mensajesDeError[tipoDeInput][error];
+        }
+    });
+    return mensaje
+}
+
+function validarNacimiento(input) {
+    if (!input.value) {
+        return
+    }
+    const fechaCliente = new Date(input.value);
+    let mensaje = "";
+    if (!mayorEdad(fechaCliente)) {
+      mensaje = "Debes tener al menos 18 años de edad"  
+    };
+    input.setCustomValidity(mensaje);
+}
+
+function mayorEdad(fecha){
+    const fechaActual = new Date();
+    const diferenciaFechas = new Date(
+        fecha.getUTCFullYear() + 18,
+        fecha.getUTCMonth(),
+        fecha.getUTCDate()
+    );
+    return diferenciaFechas <= fechaActual;
+}
